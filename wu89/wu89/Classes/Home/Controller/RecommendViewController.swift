a92740c5f90749012468421d76700dfada90b0fd
wu <wu@wudeiMac.local>
2017-06-17 16:28:09 +0800

    推荐collectionview 布局完成

--- wu89/wu89/Classes/Home/Controller/RecommendViewController.swift
+++ wu89/wu89/Classes/Home/Controller/RecommendViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 
 fileprivate let kItemMargin:CGFloat = 10
 fileprivate let kItemW = (sWidth - 3 * kItemMargin) / 2
-fileprivate let kItemH = kItemW * 3 / 4
+fileprivate let kNormalItemH = kItemW * 3 / 4
+fileprivate let kPettyItemH = kItemW * 4 / 3
 fileprivate let kHeadViewH:CGFloat = 50
 let backColor = #colorLiteral(red: 0.8822940588, green: 0.8823967576, blue: 0.882248044, alpha: 1)
 
@@ -19,7 +20,7 @@ class RecommendViewController: UIViewController {
     
     fileprivate lazy var collectionView :UICollectionView = {[unowned self] in
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: kItemW, height: kItemH)
+//        layout.itemSize = CGSize(width: kItemW, height: kNormalItemH)
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = kItemMargin
         layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
@@ -27,6 +28,7 @@ class RecommendViewController: UIViewController {
         
         let cv = UICollectionView(frame: self.view.bounds, collectionViewLayout: layout)
         cv.dataSource = self
+        cv.delegate = self
         cv.backgroundColor = UIColor.white
         
         
@@ -41,6 +43,7 @@ class RecommendViewController: UIViewController {
         
         collectionView.register(UINib(nibName: "CollectionHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "header")
         collectionView.register(UINib.init(nibName: "CollectionNormalCell", bundle: nil), forCellWithReuseIdentifier: "cell")
+        collectionView.register(UINib.init(nibName: "CollectionPrettyCell", bundle: nil), forCellWithReuseIdentifier: "petty")
         setupUI()
     }
 }
@@ -49,7 +52,7 @@ extension RecommendViewController{
         view.addSubview(collectionView)
     }
 }
-extension RecommendViewController:UICollectionViewDataSource{
+extension RecommendViewController:UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 12
     }
@@ -60,6 +63,10 @@ extension RecommendViewController:UICollectionViewDataSource{
         return 4
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.section == 1{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "petty", for: indexPath)
+            return cell
+        }
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         
         return cell
@@ -69,5 +76,10 @@ extension RecommendViewController:UICollectionViewDataSource{
         
         return headView
     }
-
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 1 {
+            return CGSize(width: kItemW, height: kPettyItemH)
+        }
+        return CGSize(width: kItemW, height: kNormalItemH)
+    }
 }
---
+++ wu89/wu89/Classes/Home/View/CollectionPrettyCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionPrettyCell.swift
+//  wu89
+//
+//  Created by wu on 2017/6/17.
+//  Copyright © 2017年 wu. All rights reserved.
+//
+
+import UIKit
+
+class CollectionPrettyCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
